165e427b715f0089069a2becd66c36e1c62a0a91
Sovandev Khawas <sovandevkhawass@gmail.com>
2024-10-18 20:44:22 +0530

    Add files via upload

---
+++ bhaban - C.php
@@ -0,0 +1,108 @@
+<?php include('common/header.php'); ?>
+
+	<section class="main-section">
+		
+		<div class="side-pannel inner-panel">
+			<div class="container-fluid">			
+				<div class="row">
+					<?php include('common/left_menu.php'); ?>
+					<div class="col-lg-9 p-lg-0">
+						<div class="banner-inner">
+							<img src="images/about-matri.jpg" class="w-100">
+							<h2>About Matri Bhavan</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="box">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-12">
+						<!-- <h1 style="text-align:center">Ramakrishna Sarada Mission Matri Bhavan</h1> -->
+						<h1  class="round position-relative" style="text-align:center">Ramakrishna Sarada Mission Matri Bhavan 
+							<span class="position-absolute " style="right: 2%; top: 50%; transform: translateY(-50%);"><img src="images/star.gif"> <a href="donation.php" class="btn btn-primary m-0"> Donate Us </a><img src="images/star.gif"></span>
+						</h1><br>
+						<p><span>Ramakrishna Sarada Mission MatriBhavan, <b>the only Charitable Hospital centre</b> run under the auspices of the Ramakrishna Sarada Mission, Dakshineswar, Kolkata – 700076 has been rendering dedicated services   women and childrenirrespective of religion, caste or creedthrough it’s Charitable Hospital, School of Nursing, Community Health Services and Vocational Training Unit, inspired by Swami Vivekananda’s Ideal <b>“Service to Man is Service to God”.</b></span><span>It was started in 1950 by Ramakrishna Mission, Belur in the residence of Late Ramaprasad Roy with only 10 beds. In November 1961, the management of Matri Bhavan was handed over to the Ramakrishna Sarada Mission the women’s counterpart of the Ramakrishna Order. Since then it is being entirely managed by the Governing Body of the above mission and the hospital is known as Ramakrishna Sarada Mission Matri Bhavan.</span><span>From a humble origin,  MatriBhavan gradually grown up to its present stature of a 100 bedded charitable hospital with 80 beds for  Obstetrics ,Gynae, General Medical, General Surgery and Peadiatrics and  20 for Neonates providing health care to economically deprived section  and middle-class mothers and children and promoting the concepts of safe motherhood among the under privileged families.</span><span>It also promotes holistic care of the family through a range of community activities targeting to achieve self-reliance and self-confidence among mothers and children.</span><span>The Institution celebrated its Silver Jubilee in 1975 and the Golden Jubilee in 2000.<br>Matri Bhavan has crossed many a hurdle since its inception but without deviating from the core principle of Swami Vivekananda’s Ideal, “Work is Worship”, serving mankind as manifestation of the Divine, irrespective of caste, creed and nationality.</span> </p>
+						<p><span>The Ramakrishna Sarada Mission Matri Bhavan is mainly functioning with the donated funds, collected from various organizations / individuals. Recently, for running day to day charitable hospital activities, the <b> INFOSYS FOUNDATION</b> has been contributing a major role towards generating the funds since 2021-2022.</span></p>
+						<h1 style="text-align:center" class="green-box p-3">Secretary Desk</h1>
+						<p><span>Welcome to Ramakrishna Sarada Mission MatriBhavan, an esteemed charitable hospital nestled in the heart of Kolkata, dedicated to the well-being of women and children regardless of their background. Founded in 1950 under the auspices of Ramakrishna Mission Belur, MatriBhavan began its noble journey with just 10 beds in the residence of Late Ramaprasad Roy. In November 1961, the management baton was gracefully passed to the Ramakrishna Sarada Mission, the esteemed women's counterpart of the Ramakrishna Order. Since then, MatriBhavan has blossomed under the caring wings of the Mission's Governing Body, embodying the ideals of Swami Vivekananda, particularly the timeless axiom, <b>"Service to Man is Service to God"</b>.<br><br>
+						From its modest beginnings, MatriBhavan has evolved into a beacon of hope, now boasting a 100-bed charitable hospital. With 80 beds dedicated to Obstetrics, Gynecology, General Medicine, General Surgery, and Pediatrics, alongside 20 beds for Neonatal care, we cater to the healthcare needs of economically disadvantaged and middle-class mothers and children. Our mission extends beyond medical care; we strive to instill the concept of safe motherhood within underprivileged families, fostering holistic family well-being.<br><br>
+						Celebrating milestones of service, MatriBhavan commemorated its Silver Jubilee in 1975 and its Golden Jubilee in 2000, marking decades of unwavering commitment to humanity. Despite facing numerous challenges along the way, MatriBhavan has remained steadfast in its adherence to Swami Vivekananda's principle that <b>"Work is Worship"</b>, viewing service to mankind as the purest form of devotion, transcending barriers of caste, creed, and nationality.<br><br>
+						Join us in our journey of compassion and healing, where every act of kindness is a tribute to the divine within us all. Explore our website to learn more about our charitable endeavors, community outreach initiatives, and the transformative impact we strive to make in the lives of women and children.<br><br>
+						<b>
+						Secretary Mataji <br>
+                        Ramakrishna Sarada Mission MatriBhavan<br>
+						</b>
+					    </span> </p>
+						<h3 class="round" style="text-align:center">Click on the YouTube icon below; it will redirect you to our YouTube channel.</h3>
+						
+						<a href="https://www.youtube.com/@rksmmatribhavan1178/videos" target="_blank" class="text-danger font-weight-bold d-block text-center"><i class="ti-youtube display-4"></i> </a>
+
+						<div class="col-lg-12 text-center mt-4">
+                           <a href="docs/about_bhavan.pdf" class="btn btn-primary" target="_blank">For more information click Here</a>
+					    </div>	
+
+						<div class="col-lg-12 text-center mt-4">
+                           <a href="https://www.telegraphindia.com/west-bengal/upgrade-boost-for-mission-hospital/cid/1288403" class="btn btn-primary" target="_blank">Click here to read an article in The Telegraph about Matri Bhavan.</a>
+					    </div>	
+						
+						<div class="col-lg-12 text-center mt-4">
+							<a href="https://srisaradamath.org/matribhavan.php" class="btn btn-primary" target="_blank">Click here to know more about Matri Bhavan on the Sarada Math website</a>
+						</div>
+					</div>
+				</div>
+				<br>
+                <br>
+				<div class="row">
+					<div class="col-lg-12">
+						<h2 style="text-align:center" class="green-box p-3">Managing Committee</h2>
+						<div class="box-table p-3">							
+							
+							<ul style="text-align:center">
+							    <b><u>President</u></b><br>Sri Shiblal Bose <br><br>
+
+								<b><u>Vice-President</u></b><br>Justice Soumen Sen <br> Dr. Sabyasachi Sen,IAS(Retd.) <br><br>
+                                
+								<b><u>Secretary</u></b><br>Pravrajika Anirvanaprana<br><br>
+
+								<b><u>Asst. Secretary</u></b><br>Pravrajika Anilaprana <br> Pravrajika Alokaprana<br><br>
+
+								<b><u>Treasurer</u></b><br>Pravrajika Yogeshaprana<br><br>
+
+								<b><u>Member</u></b><br>Pravrajika Jnanadaprana <br> Sri Pradip Kumar Mahanti <br> Dr. Prosenjit Sarkar <br> Smt. Jhantu Datta Chaudhuri<br>
+							</ul>
+							
+						</div>
+					</div>
+				</div>
+				<br>
+				<br>
+                <div class="row">
+					<div class="col-lg-12">
+						<h2 style="text-align:center" class="green-box p-3">Free Community Services</h2><br>
+						<div class="box-table p-3">							
+							<h3>Community Health Services</h3><br>
+							<ul>
+								<li><b>Out Reach Medical Camps –</b> General Health Check up, Diabetes , Anaemia , Hypertionsion Screening, Free treatment, free distribution of medicines.</li>
+								<li><b>Daily home visit,</b> discussing & picking up health problems identifying & referring high risk cases, follow up , free distribution of medicines.</li>
+								<li><b>Generating awareness</b> on Institutional delivery, Immunisation, Family Planning, Antenatal, Postnatal &Newborn care and the Govt.  Health scheme available for them.</li>
+								<li><b>Continuous Education</b> & <b>Counselling</b> on Heath & Hygiene issues like cleanliness,  Malnutrition, prevention & control of Communicable, Noncommunicable diseases etc.</li>
+								<li><b>Distributing IEC material,</b> Health Educational booklets.</li>
+								<li><b>Continuous Awareness</b> & Counselling on health Precaution, Prevention  & Control of  <b>Covid 19</b></li>
+							</ul>
+							<h3>Cancer Care Camp</h3>
+							<p><b>Awareness, Prevention, Control, Screening</b> & Treatment of Breast, Cervical & Oral Cancer for Women - Bishnupur Block II, South 24 PGS.</p>
+							<ul>
+								<li>Cervical Swab Test & Colposcopy, Mammography, Oral Investigation.</li>
+								<li>Awareness, Counselling, Screening, Self Breast Examination teaching.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				
+			</div>
+		</div>
+	</section>	
+<?php include('common/footer.php'); ?>
